7e80f0484049d7ec82a66897039ef60ad1c7ec9f
Thomas Christian <thomas@coxinha.(none)>
2015-08-20 11:16:07 -0300

    Need to be developed: Script to calculate the divergence in the Ribeirao Das Posses

---
+++ divergence.py
@@ -0,0 +1,25 @@
+# Thomas 23 July
+# Calculate the divergence between a group of stations 
+
+
+from LCBnet_lib import *
+import geopy
+
+
+def Divergence():
+    def __init__(self):
+
+att=att_sta()
+att.getatt('C15','Lat')
+
+stanames=att.stations(['Head'])
+stalat=att_sta().sortsta(stanames,'Lat')['position']
+
+
+
+if __name__ == "__main__":
+
+
+
+
+
